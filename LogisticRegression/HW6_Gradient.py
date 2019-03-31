09b035bc5a762a281509d9992c284fc7be69c1ef
PureDevPer <wooseok.kim1207@gmail.com>
2019-03-31 17:17:38 -0400

    Logistic Regression

--- LogisticRegression/HW6_Gradient.py
+++ LogisticRegression/HW6_Gradient.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 from numpy.linalg import inv
 
 class LogisticRegression:
-    def __init__(self, learning_rate=0.01, threshold=0.01, iterations=10, train_intercept=True):
+    def __init__(self, learning_rate=0.01, threshold=0.01, iterations=10000, train_intercept=True, verbose=False):
         self._learning_rate = learning_rate  # learning rate
-        self._iterations = iterations  # number of iteration
+        self._iterations = iterations  # number of iterations
         self._threshold = threshold  # threshold
         self._train_intercept = train_intercept  # whether to use intercept
-        
+        self._verbose = verbose  # Whether to print ongoing process
 
     # return theta coefficient
     def get_theta(self):
@@ -25,6 +25,7 @@ class LogisticRegression:
     def cost(self, h, y):
         return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
 
+
     def train(self, x_data, y_data):
         totalNum, totalCol = np.shape(x_data)
 
@@ -33,37 +34,37 @@ class LogisticRegression:
 
         
         self._theta = np.zeros(x_data.shape[1])
-        x_data = np.transpose(x_data)
-        y_data = np.array(y_data)
-        
 
         for i in range(self._iterations):
-            hypothesis = self.sigmoid(np.dot(np.transpose(self._theta), x_data))
+            hypothesis = self.sigmoid(np.dot(x_data, self._theta))
 
             # difference between hypothesis and real data
             diff = hypothesis - y_data
 
             # cost function
             cost = self.cost(hypothesis, y_data)
-            H = np.dot(hypothesis*(1 - hypothesis)*(x_data), x_data.T) / totalNum
-            dJ = np.sum(diff*x_data, axis=1) / totalNum
-            gradient = np.dot(inv(H), dJ)
+            gradient = np.dot(np.transpose(x_data), diff) / totalNum
 
-            # Update theta
-            self._theta = self._theta - np.transpose(np.array([gradient]))
+            # Update theta using gradient
+            self._theta -= self._learning_rate * gradient
 
-            print('cost :', cost, '\t theta: ', self._theta[:, 0])
+            # Terminate the loop when cost < threshold
+            if cost < self._threshold:
+                return False
 
+            # Print cost and theta when loop % 100 == 0
+            if (self._verbose == True and i % 100 == 0):
+                print('cost :', cost, '\t theta: ', self._theta)
 
     def plotGraph(self, x_data, y_data, theta):
         plt.plot(x_data[np.flatnonzero(y_data == 1), 0], x_data[np.flatnonzero(y_data == 1), 1], 'ro')
         plt.plot(x_data[np.flatnonzero(y_data == 0), 0], x_data[np.flatnonzero(y_data == 0), 1], 'bo')
         plotX = [np.ndarray.min(x_data), np.ndarray.max(x_data)]
-        plotY = np.subtract(np.multiply(-(theta[2][0]/theta[1][0]), plotX), theta[0][0]/theta[1][0])
+        plotY = np.subtract(np.multiply(-(theta[2]/theta[1]), plotX), theta[0]/theta[1])
         plt.plot(plotX, plotY, 'g-')
-        plt.title("Logistic Regression - Newton's method")
+        plt.title("Logistic Regression - Gradient Descent")
         plt.show()
-    
+  
 
 
 if __name__ == "__main__":
@@ -71,7 +72,8 @@ if __name__ == "__main__":
     y = np.loadtxt('hw6y.dat')
 
     # Training
-    model = LogisticRegression(learning_rate=0.1, threshold=0.01, iterations=10)
+    model = LogisticRegression(learning_rate=0.1, threshold=0.01, iterations=10000, verbose=True)
     model.train(X, y)
-    print(model.get_theta()[:,0])
+    print(model.get_theta())
     model.plotGraph(X, y, model.get_theta())
+
