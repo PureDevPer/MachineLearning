86109eda8167263d4645a1c51eeb0ad14a9538e3
PureDevPer <wooseok.kim1207@gmail.com>
2019-04-24 21:26:46 -0400

    Ensemble

---
+++ Project - EMNIST/Handwritten-alphabet(ensemble).py
@@ -0,0 +1,160 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+import tensorflow as tf
+
+
+train = pd.read_csv('emnist/emnist-balanced-train.csv', header=None)
+#train = pd.read_csv('emnist/emnist-digits-train.csv', header=None)
+
+test = pd.read_csv('emnist/emnist-balanced-test.csv', header=None)
+#test = pd.read_csv('emnist/emnist-digits-test.csv', header=None)
+
+#train.head()
+
+# Number of Train
+numTrain = len(train)
+# Number of Test
+numTest = len(test)
+# Number of Classes
+numClasses = len(train[0].unique())
+learning_rate = 0.001
+
+train_data = train.iloc[:, 1:]
+train_labels = train.iloc[:, 0]
+test_data = test.iloc[:, 1:]
+test_labels = test.iloc[:, 0]
+
+# one-hot encoding
+train_labels = pd.get_dummies(train_labels)
+test_labels = pd.get_dummies(test_labels)
+#train_labels.head()
+
+train_data = train_data.values
+train_labels = train_labels.values
+test_data = test_data.values
+test_labels = test_labels.values
+
+
+rand = random.randint(0, numTrain - 1)
+
+'''
+plt.imshow(
+    train_data[rand].reshape([28, 28]), 
+    cmap='Greys'
+    )
+plt.show()
+'''
+
+def rotate(image):
+    image = image.reshape([28, 28])
+    image = np.fliplr(image)
+    image = np.rot90(image)
+    return image.reshape([28 * 28])
+train_data = np.apply_along_axis(rotate, 1, train_data)/255
+test_data = np.apply_along_axis(rotate, 1, test_data)/255
+
+
+
+class Model:
+
+    def __init__(self, sess, name):
+        self.sess = sess
+        self.name = name
+        self._build_net()
+
+    def _build_net(self):
+        with tf.variable_scope(self.name):
+            self.training = tf.placeholder(tf.bool)
+
+            # input place holders
+            self.X = tf.placeholder(tf.float32, [None, 784])
+
+            # img 28x28x1 (black/white), Input Layer
+            X_img = tf.reshape(self.X, [-1, 28, 28, 1])
+            self.Y = tf.placeholder(tf.float32, [None, numClasses])
+
+            # Convolutional Layer 1 and Pooling Layer 1
+            conv1 = tf.layers.conv2d(inputs=X_img, filters=32, kernel_size=[3, 3], padding="SAME", activation=tf.nn.relu)
+            pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], padding="SAME", strides=2)
+            dropout1 = tf.layers.dropout(inputs=pool1, rate=0.3, training=self.training)
+
+            # Convolutional Layer 2 and Pooling Layer 2
+            conv2 = tf.layers.conv2d(inputs=dropout1, filters=64, kernel_size=[3, 3], padding="SAME", activation=tf.nn.relu)
+            pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], padding="SAME", strides=2)
+            dropout2 = tf.layers.dropout(inputs=pool2, rate=0.3, training=self.training)
+
+            # Convolutional Layer 3 and Pooling Layer 3
+            conv3 = tf.layers.conv2d(inputs=dropout2, filters=128, kernel_size=[3, 3], padding="SAME", activation=tf.nn.relu)
+            pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2, 2], padding="SAME", strides=2)
+            dropout3 = tf.layers.dropout(inputs=pool3, rate=0.3, training=self.training)
+
+            # Dense Layer with Relu
+            flat = tf.reshape(dropout3, [-1, 128 * 4 * 4])
+            dense4 = tf.layers.dense(inputs=flat, units=625, activation=tf.nn.relu)
+            dropout4 = tf.layers.dropout(inputs=dense4, rate=0.5, training=self.training)
+
+            # Logits (no activation) Layer: L5 Final FC 625 inputs -> outputs
+            self.logits = tf.layers.dense(inputs=dropout4, units=numClasses)
+
+        # define cost/loss & optimizer
+        self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.Y))
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.cost)
+
+        correct_prediction = tf.equal(tf.argmax(self.logits, 1), tf.argmax(self.Y, 1))
+        self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    def predict(self, x_test, training=False):
+        return self.sess.run(self.logits, feed_dict={self.X: x_test, self.training: training})
+
+    def get_accuracy(self, x_test, y_test, training=False):
+        return self.sess.run(self.accuracy, feed_dict={self.X: x_test, self.Y: y_test, self.training: training})
+
+    def train(self, x_data, y_data, training=True):
+        return self.sess.run([self.cost, self.optimizer], feed_dict={self.X: x_data, self.Y: y_data, self.training: training})
+
+# initialize
+sess = tf.Session()
+
+models = []
+num_models = 4
+for m in range(num_models):
+    models.append(Model(sess, "model" + str(m)))
+
+sess.run(tf.global_variables_initializer())
+
+print('Learning Started!')
+
+num_epochs = 15
+batch_size = 100
+print('numTrain: ', numTrain)
+num_iterations = int( numTrain / batch_size)
+
+
+# Start training model
+for epoch in range(num_epochs):
+    avg_cost_list = np.zeros(len(models))
+    for i in range(num_iterations):
+        batch_xs, batch_ys = train_data[i * 100: (i + 1) * 100], train_labels[i * 100: (i + 1) * 100]
+
+        # Start training each model
+        for m_idx, m in enumerate(models):
+            c, _ = m.train(batch_xs, batch_ys)
+            avg_cost_list[m_idx] += c / num_iterations
+
+    print('Epoch:', '%04d' % (epoch + 1), 'cost =', avg_cost_list)
+
+print('Learning Finished!')
+
+
+# Test model and check accuracy
+predictions = np.zeros([numTest, numClasses])
+for m_idx, m in enumerate(models):
+    print(m_idx, 'Accuracy:', m.get_accuracy(test_data, test_labels))
+    p = m.predict(test_data)
+    predictions += p
+
+ensemble_correct_prediction = tf.equal(tf.argmax(predictions, 1), tf.argmax(test_labels, 1))
+ensemble_accuracy = tf.reduce_mean(tf.cast(ensemble_correct_prediction, tf.float32))
+print('Ensemble accuracy:', sess.run(ensemble_accuracy))
